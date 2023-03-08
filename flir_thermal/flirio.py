9d7d9a04b219afd0cdba291ba09141927d6314e5
Hakan Kucukdereli <hkucukdereli@gmail.com>
2023-03-08 13:28:51 -0500

    Reads only a given list of frames with the option of reading them all. Added functions to calculate fps and duration.

--- flir_thermal/flirio.py
+++ flir_thermal/flirio.py
@@ -1,31 +1,31 @@
 import numpy as np
 from tqdm import tqdm
-import cv2
 import ffmpeg
 
 import fnv
-import fnv.reduce
 from fnv.file import ImagerFile
 
 class flirVideo(ImagerFile):
     def __init__(self, video_path):
         if type(video_path)!=str:
-            video_path = str(video_path)
-
-        super().__init__(video_path)
-
-        self.path = video_path
-
-        # set desired units
-        if self.has_unit(fnv.Unit.TEMPERATURE_FACTORY):
-            # set units to temperature, if available
-            self.unit = fnv.Unit.TEMPERATURE_FACTORY
-            self.temp_type = fnv.TempType.CELSIUS         # set temperature unit
-        else:
-            # if file has no temperature calibration, use counts instead
-            self.unit = fnv.Unit.COUNTS
-
+            self.video_path = str(video_path)
+            
+        # Check if the video file exists
         self.has_data = False
+        if os.path.isfile(self.video_path):
+            super().__init__(self.video_path)
+            self.has_data = True
+
+            # set desired units
+            if self.has_unit(fnv.Unit.TEMPERATURE_FACTORY):
+                # set units to temperature, if available
+                self.unit = fnv.Unit.TEMPERATURE_FACTORY
+                self.temp_type = fnv.TempType.CELSIUS         # set temperature unit
+            else:
+                # if file has no temperature calibration, use counts instead
+                self.unit = fnv.Unit.COUNTS
+        else:
+            raise IOError(f"{self.path} is not found.")
 
     @property
     def size(self):
@@ -39,36 +39,68 @@ class flirVideo(ImagerFile):
     @property
     def metadata(self):
         if self.has_data:
+            self.get_duration()
+            self.get_fps()
             print(f'Video is {self.duration} seconds long recorded at {self.fps} Hz.\nNumber of frames: {self.num_frames}, height: {self.height}, width: {self.width}')
             return None
         else:
             print('No video data found.')
             return None
 
-    def read_video(self):
-        self.data = np.zeros((self.num_frames, self.height, self.width)) + np.nan
-        self.time = []
+    def get_duration(self):
+        if self.has_data:
+            self.get_frame(0)
+            time_begin = self.frame_info.time
+            
+            self.get_frame(self.num_frames)
+            time_end = self.frame_info.time
 
-        frames = np.arange(self.num_frames)
-        for i in tqdm(frames, total=len(frames), desc='Loading video...'):
-            self.get_frame(i)
-            self.data[i] = np.array(self.final, copy=False).reshape((self.height, self.width))
-            self.time.append(self.frame_info.time)
+            self.duration = (time_begin - time_end).total_seconds() # video duration in seconds
 
-        self.duration = (self.time[-1] - self.time[0]).total_seconds() # video duration in seconds
-        fps = self.num_frames / self.duration
-        state = np.argmin([np.abs(fps-60), np.abs(fps-30)])
-        if state==0:
-            self.fps = 60
+            return self.duration
         else:
-            self.fps = 30
+            return None
 
-        if len(self.data)>0:
-            self.has_data = True
+    def get_duration(self)():
+        try:
+            self.get_duration()
 
-        return self.time, self.data
+            fps = self.num_frames / self.duration
+            
+            state = np.argmin([np.abs(fps-60), np.abs(fps-30)])
+            if state==0:
+                self.fps = 60
+            else:
+                self.fps = 30
 
-    def save_video(self, save_path, vcodec='libx264'):
+            return self.fps
+        except:
+            return None
+
+    def read_video(self, frames=[]):
+        if self.has_data:
+            data = np.zeros((frames, self.height, self.width)) + np.nan
+            time = []
+            index = np.zeros(len(frames))
+
+            if len(frames)==0:
+                frames = np.arange(self.num_frames)
+            else:
+                pass
+
+            for i in tqdm(enumerate(frames), total=len(frames), desc='Loading video...'):
+                self.get_frame(i)
+                index[i] = i
+                data[i] = np.array(self.final, copy=False).reshape((self.height, self.width))
+                time.append(self.frame_info.time)
+
+            return index, data, time
+        else:
+            print ('No video file is found.')
+
+            return None, None, None
+
+    def save_video(self, data, save_path, vcodec='libx264'):
         if self.has_data:
             process = (
                 ffmpeg
@@ -78,7 +110,7 @@ class flirVideo(ImagerFile):
                     .run_async(pipe_stdin=True)
             )
 
-            for frame in self.data:
+            for frame in data:
                 process.stdin.write(
                     frame
                         .astype(np.uint8)

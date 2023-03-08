cb2d9c98d27ddd46f36790fda36a4d3bcb67f793
Hakan Kucukdereli <hkucukdereli@gmail.com>
2023-03-08 09:24:21 -0500

    Cleaned up a mess.

--- flir_thermal/flirio.py
+++ flir_thermal/flirio.py
@@ -11,9 +11,9 @@ class flirVideo(ImagerFile):
     def __init__(self, video_path):
         if type(video_path)!=str:
             video_path = str(video_path)
-            
+
         super().__init__(video_path)
-        
+
         self.path = video_path
 
         # set desired units
@@ -24,40 +24,50 @@ class flirVideo(ImagerFile):
         else:
             # if file has no temperature calibration, use counts instead
             self.unit = fnv.Unit.COUNTS
-        
+
         self.has_data = False
-    
+
     @property
     def size(self):
         if self.has_data:
-            return f'Num frames: {self.num_frames}, height: {self.height}, width: {self.width}'
+            print(f'Num frames: {self.num_frames}, height: {self.height}, width: {self.width}')
+            return None
         else:
             print('No video data found.')
             return None
-        
+
+    @property
+    def metadata(self):
+        if self.has_data:
+            print(f'Video is {self.duration} seconds long recorded at {self.fps} Hz.\nNumber of frames: {self.num_frames}, height: {self.height}, width: {self.width}')
+            return None
+        else:
+            print('No video data found.')
+            return None
+
     def read_video(self):
         self.data = np.zeros((self.num_frames, self.height, self.width)) + np.nan
         self.time = []
-        
+
         frames = np.arange(self.num_frames)
         for i in tqdm(frames, total=len(frames), desc='Loading video...'):
             self.get_frame(i)
             self.data[i] = np.array(self.final, copy=False).reshape((self.height, self.width))
             self.time.append(self.frame_info.time)
-            
+
         self.duration = (self.time[-1] - self.time[0]).total_seconds() # video duration in seconds
         fps = self.num_frames / self.duration
-        state = np.argmin([np.abs(fps-60), np.abs(fps-29.97)])
+        state = np.argmin([np.abs(fps-60), np.abs(fps-30)])
         if state==0:
             self.fps = 60
         else:
-            self.fps = 30       
-            
+            self.fps = 30
+
         if len(self.data)>0:
             self.has_data = True
-                
+
         return self.time, self.data
-    
+
     def save_video(self, save_path, vcodec='libx264'):
         if self.has_data:
             process = (
@@ -78,4 +88,3 @@ class flirVideo(ImagerFile):
             process.wait()
         else:
             raise MissingValueError('Video data has not been loaded. Run < read_video > first.')
-            
